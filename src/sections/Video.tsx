4c9227891937147cb70eecddefce70e5d366523e
xiachengguang <xiachengguang@163.com>
2026-02-13 17:17:55 +0800

    Update Video.tsx

--- src/sections/Video.tsx
+++ src/sections/Video.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useLanguage } from '@/context/LanguageContext';
+import { useLanguage } from './context/LanguageContext';
 import { Play, X } from 'lucide-react';
 
 export default function Video() {
