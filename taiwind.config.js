1c0044b5f911920c100bacffd71f11e8f050d04b
xiachengguang <xiachengguang@163.com>
2026-02-13 09:39:15 +0800

    Update taiwind.config.js

--- taiwind.config.js
+++ taiwind.config.js
@@ -22,5 +22,5 @@ module.exports = {
       borderRadius: { lg: "var(--radius)", md: "calc(var(--radius) - 2px)", sm: "calc(var(--radius) - 4px)" },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 }
