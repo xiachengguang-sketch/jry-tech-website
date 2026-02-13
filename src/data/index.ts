222227691fee99449a80d3650c0889d815a8950f
xiachengguang <xiachengguang@163.com>
2026-02-13 09:11:19 +0800

    Create index.ts

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+export { products, allProducts } from './products';
+export { solutions } from './solutions';
+export { advantages } from './advantages';
+export { blogPosts } from './blog';
+export { stats, clients } from './stats';
