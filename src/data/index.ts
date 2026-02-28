d6c5085a85063f5b98dd1c47ff8e50123869f684
xiachengguang <xiachengguang@163.com>
2026-02-28 09:39:19 +0800

    Delete src directory

--- src/data/index.ts
+++
@@ -1,5 +0,0 @@
-export { products, allProducts } from './products';
-export { solutions } from './solutions';
-export { advantages } from './advantages';
-export { blogPosts } from './blog';
-export { stats, clients } from './stats';
